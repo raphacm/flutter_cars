3d14290fc1be25e498fd18b2021d0af0b08335ca
Raphael Costa <raphael.costa@m4u.com.br>
2020-08-18 01:07:58 -0300

    initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      child: Center(
+        child: Text("Carros"),
+      ),
+    );
+  }
+}
